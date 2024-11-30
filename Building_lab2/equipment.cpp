89d1d4d4e3e3d2ec28bbc0042d5204a2f3b92ada
2Yana7 <144928743+2Yana7@users.noreply.github.com>
2024-11-30 14:18:22 +0300

    Delete Building_lab2 directory

--- Building_lab2/equipment.cpp
+++
@@ -1,8 +0,0 @@
-#include "equipment.h"
-#include <iostream>
-
-equipment::equipment(const string& type, int id) : type(type), id(id) {}
-
-void equipment::operate() const {}
-string equipment::get_type() const { return type; }
-int equipment::get_id() const { return id; }
--- Building_lab2/material.cpp
+++
@@ -1,6 +0,0 @@
-#include "material.h"
-
-material::material(const string& name, double cost) : name(name), cost(cost) {}
-
-string material::get_name() const { return name; }
-double material::get_cost() const { return cost; }
